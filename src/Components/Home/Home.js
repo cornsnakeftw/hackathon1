e54126294260045a923bcf44b2d6dad43ad2ce70
cornsnakeftw <marianne@besquare.com.my>
2022-07-22 02:04:51 +0800

    update UI on homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,33 @@
 import React from "react";
 import axios from "axios";
-import "./index.css";
-import snowflake from "./snowflake.png"
+import "./home.css";
+import snowflake from "./snowflake.png";
 import DataDisney from "../Characters/DataDisney";
-
 
 const Home = () => {
   return (
-    <section className="body">
-      <nav className="home">
+    <section>
+      <nav>
+        {/* <div className="animate">
+          <img src={snowflake} alt="snowflake" />
+          <img src={snowflake} alt="snowflake" />
+          <img src={snowflake} alt="snowflake" />
+        </div>
+        <div className="animate2">
+          <img src={snowflake} alt="snowflake" />
+          <img src={snowflake} alt="snowflake" />
+        </div> */}
+        {/* <div className="content">
+          <h1 className="welcome">Welcome to Disneyland !</h1>
+          <h2 classname="h2">Where all your dream comes true</h2>
+
+          <button onClick={DataDisney} className="btn">
+            EXPLORE OUR MAGICAL WORLD
+          </button>
+          <p className="footer">© 2022 D-JAMZ Group</p>
+        </div> */}
+      </nav>
+      <div class="content">
         <div className="animate">
           <img src={snowflake} alt="snowflake" />
           <img src={snowflake} alt="snowflake" />
@@ -18,16 +37,25 @@ const Home = () => {
           <img src={snowflake} alt="snowflake" />
           <img src={snowflake} alt="snowflake" />
         </div>
-        <div className="content">
-          <h1 className="welcome">Welcome to Disneyland !</h1>
-          <h2>Where all your dream comes true</h2>
-
-          <button onClick={DataDisney} className="btn">
-            EXPLORE OUR MAGICAL WORLD
-          </button>
-          <p className="footer">© 2022 D-JAMZ Group</p>
+        <div className="text">
+          <h1 className="h1">
+            Welcome to <span class="span">Disneyland!</span>
+          </h1>
+          <h2 className="h2">Where all your dream comes true</h2>
+          <div className="buttons">
+            <button className="btn explore_btn" onClick={DataDisney}>
+              EXPLORE MAGIC
+            </button>
+          </div>
         </div>
-      </nav>
+      </div>
+      {/* <div class="footer-links">
+        <h1 className="footer_contact">Contact
+          <p className="footer_email">Email:d-jamz@gmail.com</p>
+        </h1>
+      </div> */}
+
+      <p className="footer_copyright">© 2022 D-JAMZ Group</p>
     </section>
   );
 };
