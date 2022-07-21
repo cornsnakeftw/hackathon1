679b95857df4308a2986a448eed627d1a767e367
cornsnakeftw <marianne@besquare.com.my>
2022-07-21 16:19:21 +0800

    updated styles

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,7 @@ import "./index.css";
 import snowflake from "./snowflake.png"
 import DataDisney from "../Characters/DataDisney";
 
+
 const Home = () => {
   return (
     <section className="body">
@@ -20,6 +21,7 @@ const Home = () => {
         <div className="footer">
           <h1 className="welcome">Welcome to Disneyland !</h1>
           <h2>Where all your dream comes true</h2>
+         
           <button onClick={DataDisney} className="characterPage">EXPLORE OUR MAGICAL WORLD</button>
           <p>© 2022 D-JAMZ Group</p>
         </div>
